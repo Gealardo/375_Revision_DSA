3b1956372ff2154d82dd8ecb6159b380cb2114ac
Gealardo <ayunverma902@gmail.com>
2026-09-06 11:42:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Search in Rotated Array 2/search-in-rotated-array-2.cpp
@@ -0,0 +1,30 @@
+class Solution {
+  public:
+     bool search(vector<int>& arr, int key) {
+         int n=arr.size();
+            int l=0;
+            int h=n-1;
+            while(l<=h){
+                int m=l+(h-l)/2;
+                if(arr[m]==key) return true;
+                if(arr[l]==arr[m] && arr[m]==arr[h]) {
+                    l++;h--;
+                    continue;
+                }
+                if(arr[l]<=arr[m]){
+                    if(arr[l]<=key && arr[m]>key){
+                        h=m-1;
+                    }
+                    else l=m+1;
+                }
+                else {
+                    if(arr[m]<key && arr[h]>=key){
+                        l=m+1;
+                    }
+                    else h=m-1;
+                }
+            }
+            return false;     
+         
+     }
+};
