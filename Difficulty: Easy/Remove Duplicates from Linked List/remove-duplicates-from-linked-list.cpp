ce5a64a01de40e2833f9173bd28de283b0bf4e13
Gealardo <ayunverma902@gmail.com>
2026-09-09 20:09:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Remove Duplicates from Linked List/remove-duplicates-from-linked-list.cpp
@@ -0,0 +1,46 @@
+/* Structure of linked list Node
+class Node {
+public:
+    int data;
+    Node *next;
+    Node(int x) {
+        data = x;
+        next = nullptr;
+    }
+};*/
+class Solution {
+  public:
+    void del(Node*&head,Node*&curr,Node*&prev,Node*&front){
+        if(curr==head){
+            Node* temp=curr;
+            head=front;
+            prev=curr;
+            curr=front;
+            front=front->next;
+            delete(temp);
+        }
+        else{
+            Node*temp=curr;
+            prev->next=front;
+            curr=front;
+            if(front!=nullptr) front=front->next;
+            delete(temp);
+        }
+    }
+    Node* removeDuplicates(Node* head) {
+        Node* prev=NULL;
+        Node* curr=head;
+        unordered_map<int,int>mp;
+        if(curr==NULL || curr->next==NULL) return head;
+        while(curr!=NULL){
+            Node* front = curr->next;
+            if(mp.find(curr->data)!=mp.end()){
+                del(head,curr,prev,front);
+                continue;
+            }
+            mp[curr->data]++;
+            prev=curr; curr=front;
+        }
+       return head;
+    }
+};
